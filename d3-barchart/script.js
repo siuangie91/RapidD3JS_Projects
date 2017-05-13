7f682be4f816b6964e5479b0d4786479f248ad72
siuangie91 <siuangie91@gmail.com>
2017-05-13 15:42:10 -0400

    move folders of projects from stackskills into a stackskills_projects folder; in d3-barchart, begin creating <svg> chart

--- d3-barchart/script.js
+++ d3-barchart/script.js
@@ -1,5 +1,8 @@
 var data = [4, 8, 15, 16, 23, 42];
 
+//*********** HTML BAR CHART *********
+/*
+
 // Produce a linear scale to map appropriate bar width.
 // scales specify a mapping from data space (domain) to display space (range)
 // x is also a function here!
@@ -14,3 +17,38 @@ d3.select('.chart')
         .style("width", function(d) { return x(d) + "px"; })
         .text(function(d) { return d; });
 
+*/
+
+//*********** SVG BAR CHART *********
+var width = 420,
+    barHeight = 20;
+
+var x = d3.scaleLinear()
+    .domain([0, d3.max[data]])
+    .range([0, d3.max[data] * 10])
+
+var chart = d3.select("svg.chart")
+    .attr("width", width)
+    .attr("height", barHeight * data.length); // set <svg> height based on size of dataset; size is based on the height of each bar rather than the overall height of the chart
+
+var bar = d3.selectAll("g")
+        .data(data)
+    .enter().append("g") // append <g>s (svg groups) using data join
+        .attr("transform", function(d,i) { return "translate(0, " + i * barHeight + ")"}); // translate <g> elem along y-axis, creating a local origin for positioning the bar and its label
+
+// Since there is exactly one <rect> and one <text> per <g>,
+// we can append these elements directly to the <g>,
+// without needing additional data joins.
+// Data joins are only needed when creating a variable number of children
+// based on data; here we are appending just one child per parent.
+// The appended <rect>s and <text>s inherit data from their parent g element, and thus we can use data to compute the bar width and label position.
+
+bar.append("rect") // append <rect> to each <g>
+    .attr("width", x)
+    .attr("height", barHeight - 1)
+
+bar.append("text") // append <text> to each <g>
+    .attr("x", function(d) { return x(d) - 3; })
+    .attr("y", barHeight / 2)
+    .attr("dy", ".35em")
+    .text(function(d) { return d; });
