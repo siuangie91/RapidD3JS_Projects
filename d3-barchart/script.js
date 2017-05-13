517795744f23f8f1f9a31a2516a6ecd2909e1bb3
siuangie91 <siuangie91@gmail.com>
2017-05-13 15:55:34 -0400

    refactor code to use .tsv file

--- d3-barchart/script.js
+++ d3-barchart/script.js
@@ -1,4 +1,4 @@
-var data = [4, 8, 15, 16, 23, 42];
+//var data = [4, 8, 15, 16, 23, 42];
 
 //*********** HTML BAR CHART *********
 /*
@@ -23,32 +23,55 @@ d3.select('.chart')
 var width = 420,
     barHeight = 20;
 
-var x = d3.scaleLinear()
-    .domain([0, d3.max[data]])
-    .range([0, d3.max[data] * 10])
+var x = d3.scaleLinear() //.domain([0, d3.max[data]]) // data now pulled from tsv
+    .range([0, /*d3.max[data] * 10*/ width])
 
 var chart = d3.select("svg.chart")
-    .attr("width", width)
-    .attr("height", barHeight * data.length); // set <svg> height based on size of dataset; size is based on the height of each bar rather than the overall height of the chart
+    .attr("width", width);
+    // move the setting of height to tsv() function
+    // .attr("height", barHeight * data.length); // set <svg> height based on size of dataset; size is based on the height of each bar rather than the overall height of the chart
 
-var bar = d3.selectAll("g")
+d3.tsv("data.tsv", type, function(error,data) {
+    x.domain([0, d3.max(data, function(d) { return d.Value; })]);
+
+    chart.attr("height", barHeight * data.length);
+
+    var bar = d3.selectAll("g")
         .data(data)
     .enter().append("g") // append <g>s (svg groups) using data join
         .attr("transform", function(d,i) { return "translate(0, " + i * barHeight + ")"}); // translate <g> elem along y-axis, creating a local origin for positioning the bar and its label
 
-// Since there is exactly one <rect> and one <text> per <g>,
-// we can append these elements directly to the <g>,
-// without needing additional data joins.
-// Data joins are only needed when creating a variable number of children
-// based on data; here we are appending just one child per parent.
-// The appended <rect>s and <text>s inherit data from their parent g element, and thus we can use data to compute the bar width and label position.
+/*
+Since there is exactly one <rect> and one <text> per <g>, we can append these elements directly to the <g>, without needing additional data joins. Data joins are only needed when creating a variable number of children based on data; here we are appending just one child per parent. The appended <rect>s and <text>s inherit data from their parent g element, and thus we can use data to compute the bar width and label position.
+*/
 
-bar.append("rect") // append <rect> to each <g>
-    .attr("width", x)
-    .attr("height", barHeight - 1)
+    bar.append("rect") // append <rect> to each <g>
+        .attr("width", x)
+        .attr("height", barHeight - 1)
 
-bar.append("text") // append <text> to each <g>
-    .attr("x", function(d) { return x(d) - 3; })
-    .attr("y", barHeight / 2)
-    .attr("dy", ".35em")
-    .text(function(d) { return d; });
+    bar.append("text") // append <text> to each <g>
+        .attr("x", function(d) { return x(d) - 3; })
+        .attr("y", barHeight / 2)
+        .attr("dy", ".35em")
+        .text(function(d) { return d; });
+});
+
+function type(d) {
+    d.Value = +d.Value; // coerce to number
+    return d;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
