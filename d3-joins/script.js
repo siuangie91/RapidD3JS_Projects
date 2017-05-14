7085c5e725c61b790e997551a81d55f62c9ea999
siuangie91 <siuangie91@gmail.com>
2017-05-14 14:58:08 -0400

    comment methods

--- d3-joins/script.js
+++ d3-joins/script.js
@@ -5,11 +5,12 @@ var getRandomInt = function() {
 var svg = d3.select("svg");
 
 d3.json("data.json", function(error, data) {
-    svg.selectAll("circle")
-        .data(data)
-        .enter().append("circle")
-            .attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; })
+    svg.selectAll("circle") // 1. Returns a new empty selection, since the SVG container was empty
+        .data(data) // 2. DATA JOIN: Selection is then joined to an array of data, resulting in three new selections that represent the three possible states: enter, update, and exit. Since the selection was empty, the update and exit selections are empty, while the enter selection contains a placeholder for each new datum.
+        .enter() // 3. The update selection is returned by selection.data, while the enter and exit selections hang off the update selection; selection.enter thus returns the enter selection.
+        .append("circle") // 4. The missing elements are added to the SVG container by calling selection.append on the enter selection. This appends a new circle for each data point to the SVG container.
+            .attr("cx", function(d) { return +d.x; })
+            .attr("cy", function(d) { return +d.y; })
             .style("fill", function() { return "rgb(" + getRandomInt() + "," + getRandomInt() + "," + getRandomInt() + ")"; })
             .attr("r", 25);
 });
