328378755a16992c3293dc2332fcfd2179b87cc7
siuangie91 <siuangie91@gmail.com>
2017-05-15 00:44:11 -0400

    document UPDATE/ENTER/EXIT pattern with merge to automatically update all elems, including existing ones

--- d3-joins/script.js
+++ d3-joins/script.js
@@ -2,51 +2,67 @@ var getRandomInt = function() {
     return Math.floor(Math.random() * 255);
 };
 
-var oldData = [{x: 54, y: 78}, {x: 154, y: 178}, {x: 254, y: 278}, {x: 354, y: 378}, {x: 454, y: 478}];
+var arr = [
+    {x: 54, y: 78},
+    {x: 154, y: 178},
+    {x: 254, y: 278},
+    {x: 654, y: 378},
+    {x: 454, y: 478},
+    {x: 24, y: 38},
+    {x: 724, y: 108},
+    {x: 204, y: 218},
+    {x: 904, y: 328},
+    {x: 834, y: 418},
+    {x: 36, y: 98}
+];
 
 var svg = d3.select("svg");
 
-svg.selectAll("circle")
-        .data(oldData)
-    .enter().append("circle")
-    .attr("cx", function(d) { return +d.x; })
-    .attr("cy", function(d) { return +d.y; })
-    .attr("r", 10)
-    .style("fill", "steelblue");
+function update(data, fill) {
+    fill = fill || "steelblue";
+    // DATA JOIN
+    var circle = svg.selectAll("circle")
+        .data(data);
 
-var update = function() {
-    d3.json("data.json", function(error, data) {
-// Handle ENTER selection --> static visuals
-/*
-    svg.selectAll("circle") // 1. Returns a new empty selection, since the SVG container was empty
-        .data(data) // 2. DATA JOIN: Selection is then joined to an array of data, resulting in three new selections that represent the three possible states: enter, update, and exit. Since the selection was empty, the update and exit selections are empty, while the enter selection contains a placeholder for each new datum.
-        .enter() // 3. The update selection is returned by selection.data, while the enter and exit selections hang off the update selection; selection.enter thus returns the enter selection.
-        .append("circle") // 4. The missing elements are added to the SVG container by calling selection.append on the enter selection. This appends a new circle for each data point to the SVG container.
+    // Manually UPDATE old elems as needed
+    /*
+    circle.transition().duration(800)
+        .attr("fill", "blue")
+        .attr("cx", function(d) { return +d.x; }) // reposition the existing <circle>
+        .attr("cy", function(d) { return +d.y; }) // reposition the existing <circle>
+    */
+
+    // Automatically UPDATE existin elems when creating new ones
+    // ENTER to create new elems as needed + automatically UPDATE existing elements
+    circle.enter().append("circle")
+        .merge(circle) // merge() merges arrays into a single array. In this case, it merges selections (our circle data) together.
+            .attr("r", 0)
+        .transition().duration(800).delay(function(d,i) { return i * 400; })
+            .attr("r", 10)
             .attr("cx", function(d) { return +d.x; })
             .attr("cy", function(d) { return +d.y; })
-            .style("fill", function() { return "rgb(" + getRandomInt() + "," + getRandomInt() + "," + getRandomInt() + ")"; })
-            .attr("r", 25);
-*/
+            .style("fill", fill);
 
-// Handle UPDATE, ENTER, and EXIT states
+    // EXIT to remove old elements.
+    circle.exit().transition().duration(800)
+        .attr("r", 0)
+        .remove();
+}
 
-        var circle = svg.selectAll("circle")
-            .data(data);
-
-        circle.exit().transition()
-            .attr("r", 0)
-            .remove(); // if new dataset is smaller than old one, surplus elems are removed.
-
-        circle.enter().append("circle") // if new dataset is larger, surplus elems are appended. After call to append, ENTER selection is merged with UPDATE selection
-                .attr("r", 0) // transStart state
-            .transition().duration(800).delay(function(d,i) { return i * 400; }) // declare transition
-                .attr("r", 25) // transEnd state
-                .attr("cx", function(d) { return +d.x; })
-                .attr("cy", function(d) { return +d.y; })
-                .style("fill", function() { return "rgb(" + getRandomInt() + "," + getRandomInt() + "," + getRandomInt() + ")"; });
-
-
+function updateWithJson(data, fill) {
+    d3.json(data, function(error, data) {
+        update(data, fill);
     });
-};
+}
 
-setTimeout(update, 1000);
+setTimeout(function() {
+    console.log("Update using array");
+    update(arr, "red");
+}, 1000);
+
+setTimeout(function() {
+    console.log("Update using json");
+    updateWithJson("data.json", "green");
+}, 8000);
+
+
